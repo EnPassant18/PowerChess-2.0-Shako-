b85ac56e6413ff8579da3cfc31b225df571080c4
Katie Normandin <57kkate@gmail.com>
2018-04-16 22:32:01 -0400

    moved GameDummy to main, added test file to fill

---
+++ src/test/game/GameDummyTest.java
@@ -0,0 +1,15 @@
+package game;
+
+/**
+ * Test GameDummy class.
+ *
+ * @author knorms
+ *
+ */
+public class GameDummyTest {
+  /*
+   * TODO test get/set move, set make move, test illegal move get/reset, test
+   * attempting to make illegal move
+   */
+
+}
